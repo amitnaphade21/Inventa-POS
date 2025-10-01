38a158962414fed9829e628ec7fa2f7b400794b8
Amit <amitnaphade21@gmail.com>
2025-10-01 15:42:21 +0530

    initial commit

--- pos-backend/src/main/java/com/zosh/configrations/MailConfig.java
+++ pos-backend/src/main/java/com/zosh/configrations/MailConfig.java
@@ -13,17 +13,18 @@ public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        mailSender.setHost("smtp.gmail.com"); // or your SMTP host
         mailSender.setPort(587);
-        mailSender.setUsername("your_email@gmail.com");  // replace with your email
-        mailSender.setPassword("your_app_password");     // replace with app password
+
+        mailSender.setUsername("your-email@gmail.com");
+        mailSender.setPassword("your-app-password");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
 
         return mailSender;
     }
 }
+
